40a658a96714272fc4c9a0476fd094b23d48332c
Brice Platerrier <brice.platerrier@gmail.com>
2016-05-20 14:45:48 +0200

    Add MATLAB/Octave function to compute pixel coordinates of evenly distributed points on spherical projection

--- matlab/directions/spheresampling.m
+++ matlab/directions/spheresampling.m
@@ -2,69 +2,71 @@ clear all
 close all
 graphics_toolkit('gnuplot') % COMMENT THIS LINE IN MATLAB
 
-%%------------------------------------------------------------------------------
-%   Icosahedron method
-%-------------------------------------------------------------------------------
-
-% computing coarse bins
-N_coarse = 25;
-F_coarse = 3;
-
-% initialize coordinates
-x = [];
-y = [];
-z = [];
-
-% compute coarse bins
-points = sphere_icos1_points(F_coarse, N_coarse);
-for(i=1:length(points(1,:))),
-  if(points(3,i)<=0),
-    x = [x; points(1,i)'];
-    y = [y; points(2,i)'];
-    z = [z; points(3,i)'];
-  end
-end
-
-% compute precision
-theta_min = Inf;
-for i=1:length(x),
-  for j = i+1:length(y),
-    theta_t = acos(dot([x(i), y(i), z(i)],[x(j), y(j), z(j)]));
-    if(theta_t<theta_min),
-      theta_min = theta_t;
-    end  
-  end
-end
-
-coarse_prec = theta_min*180/pi;
-printf('Coarse precision in theta: %d \n', coarse_prec);
-
-% plot coarse bins
-figure(3)
-hold on;
-axis('square');
-plot3(x,y,z,'b.');
-[x_, y_, z_] = sphere;
-mesh(x_, y_, z_, 'Facecolor', 'b', 'Facealpha', 0.1, 'Edgecolor', 'none');
-title('Coarse bins');
-hold off;
-
-% save in text file
-coarse_bins = [x, y, z];
-dlmwrite('coarse_bins.csv', coarse_bins, 'delimiter', ',', 'precision', 6);
-
-% results
-% Precision of 15 degrees for F=4 (N=162 or N_hemi=89) 
-% Precision of 20 degrees for F=3 (N=92 or N_hemi=52)
-% Precision of 31 degrees for F=2 (N=42 or N_hemi=25)
+%%%------------------------------------------------------------------------------
+%%   Icosahedron method
+%%-------------------------------------------------------------------------------
+%
+%% computing coarse bins
+%N_coarse = 25;
+%F_coarse = 3;
+%
+%% initialize coordinates
+%x = [];
+%y = [];
+%z = [];
+%
+%% compute coarse bins
+%points = sphere_icos1_points(F_coarse, N_coarse);
+%for(i=1:length(points(1,:))),
+%  if(points(3,i)<=0),
+%    x = [x; points(1,i)'];
+%    y = [y; points(2,i)'];
+%    z = [z; points(3,i)'];
+%  end
+%end
+%
+%% compute precision
+%theta_min = Inf;
+%for i=1:length(x),
+%  for j = i+1:length(y),
+%    theta_t = acos(dot([x(i), y(i), z(i)],[x(j), y(j), z(j)]));
+%    if(theta_t<theta_min),
+%      theta_min = theta_t;
+%    end  
+%  end
+%end
+%
+%coarse_prec = theta_min*180/pi;
+%printf('Coarse precision in theta: %d \n', coarse_prec);
+%
+%% plot coarse bins
+%figure(3)
+%hold on;
+%axis('square');
+%plot3(x,y,z,'b.');
+%[x_, y_, z_] = sphere;
+%mesh(x_, y_, z_, 'Facecolor', 'b', 'Facealpha', 0.1, 'Edgecolor', 'none');
+%title('Coarse bins');
+%hold off;
+%
+%% save in text file
+%coarse_bins = [x, y, z];
+%dlmwrite('coarse_bins.csv', coarse_bins, 'delimiter', ',', 'precision', 6);
+%
+%% results
+%% Precision of 15 degrees for F=4 (N=162 or N_hemi=89) 
+%% Precision of 20 degrees for F=3 (N=92 or N_hemi=52)
+%% Precision of 31 degrees for F=2 (N=42 or N_hemi=25)
 
 %-------------------------------------------------------------------------------
 
 % Computing refined bins
 
+coarse_prec = 100;
+
 % parameters 
 N_refined = 55;
-F_refined = 30;
+F_refined = 9;
 
 % compute refined bins
 points = sphere_icos1_points(F_refined, N_refined);
