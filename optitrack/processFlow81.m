df126ad6eca594ce88c2e38d1097db89bc369578
Brice Platerrier <brice.platerrier@gmail.com>
2016-06-10 00:17:35 +0200

    Final version of the report (slightly improved results and comments)

--- optitrack/processFlow81.m
+++ optitrack/processFlow81.m
@@ -135,6 +135,9 @@ qdir = quatmultiply(quatmultiply(quatinv(quat),qdir),quat);
 % end
 % bodyDir = normc(bodyDir')';
 bodyDir = qdir(:,2:4);
+velX = bodyDir(:,1)/1000;
+velY = bodyDir(:,2)/1000;
+velZ = bodyDir(:,3)/1000;
 bodyDir = normr(bodyDir);
 
 dirX = bodyDir(:,1);
@@ -143,7 +146,7 @@ dirZ = bodyDir(:,3);
 
 %clear angRates bodyDir dir dquat i invquat pos quat quatRates QuatX QuatY QuatW QuatZ rates rotM X Y Z
 
-save('flow_81_200fps_pro.mat', 'dirX', 'dirY', 'dirZ', 'rateX', 'rateY', 'rateZ', 'Time', 'ErrorMarkers');
+save('flow_81_200fps_pro.mat', 'velX', 'velY', 'velZ', 'dirX', 'dirY', 'dirZ', 'rateX', 'rateY', 'rateZ', 'Time', 'ErrorMarkers');
 
 %% Load full data
 close all
@@ -151,32 +154,24 @@ clear all
 load('flow_81_cam_pro.mat');
 load('flow_81_200fps_pro.mat');
 
-%% Synchronize measurements (convolution)
-
-% Correlation with motion capture data
-% imax = length(rateZ(:,1));
-% jmax = length(GyroZ(:,1));
-% conv = zeros(2*jmax+1, 1);
-% for j = -jmax:jmax,
-%     for i = 1:imax,
-%         if (i-j>0 && i-j<length(rateZ(:,1))),
-%             conv(i+jmax+1) = conv(j+jmax+1) + GyroZ(i-j)*rateY(i);
-%         end
-%     end
-% end
-% delay = find(conv==max(conv)) - jmax - 1;
-
 %% Synchronize manually
+close all
 % define delay
 delay = 1445;
 
-% plot results
+%% Plot angular rates measurements
 % rates around X
 figure;
 hold on
 h1 = plot(rateZ);
 h2 = plot(GyroX(delay:length(GyroX)));
 legend([h1, h2], 'Optitrack Z','Gyroscope X');
+xlabel('Frames (~5 ms)');
+ylabel('Angular rate around X');
+ax = gca;
+ax.XLim = [0,12000];
+ax.YLim = [-15, 15];
+set(gca,'FontSize', 12);
 hold off
 
 % rates around Y
@@ -185,75 +180,302 @@ hold on
 h1 = plot(rateX);
 h2 = plot(GyroY(delay:length(GyroY)));
 legend([h1, h2], 'Optitrack Z','Gyroscope Y');
+xlabel('Frames (~5 ms)');
+ylabel('Angular rate around Y');
+ax = gca;
+ax.XLim = [0,12000];
+ax.YLim = [-12, 12];
+set(gca,'FontSize', 12);
 hold off
 
 % rates around Z
 figure;
 hold on
-plot(rateY);
-plot(GyroZ(delay:length(GyroZ)));
+h1 = plot(rateY);
+h2 = plot(GyroZ(delay:length(GyroZ)));
 legend([h1, h2], 'Optitrack Y','Gyroscope Z');
+xlabel('Frames (~5 ms)');
+ylabel('Angular rate around Z');
+ax = gca;
+ax.XLim = [0,12000];
+ax.YLim = [-15, 15];
+set(gca,'FontSize', 12);
 hold off
 
 %% Compare direction estimates
+close all;
+
 % crop data
-CamZ_ = CamZ(delay:length(CamZ));
-dirY_ = dirY(1:length(CamZ_));
+CamX_ = CamX(delay:length(CamX))';
+CamY_ = CamY(delay:length(CamY))';
+CamZ_ = CamZ(delay:length(CamZ))';
+dirX_ = dirX(1:length(CamX_));
+dirY_ = dirY(1:length(CamY_));
+dirZ_ = dirZ(1:length(CamZ_));
 
 %% Smooth motion capture measurements
-g = gausswin(200,5);
+g = gausswin(200,25);
 g = g/sum(g);
-plot(g);
 
-% dirX_ = dirX;
-% dirY_ = dirY;
-% dirZ_ = dirZ;
-
-% dirX = conv(dirX_, g, 'same');
+dirX = conv(dirX_, g, 'same');
 dirY = conv(dirY_, g, 'same');
-% dirZ = conv(dirZ_, g, 'same');
+dirZ = conv(dirZ_, g, 'same');
 
-% Plot result
+% renormalize
+dir = normr([dirX, dirY, dirZ]);
+dirX = dir(:,1);
+dirY = dir(:,2);
+dirZ = dir(:,3);
+
+%% Plot results from smoothing of motion capture measurements
+
+% direction estimate along X
+figure;
+hold on
+h1 = plot(dirZ_);
+h2 = plot(dirZ);
+legend([h1 h2], 'Raw', 'Smoothed');
+ylabel('Direction estimate along X');
+xlabel('Frames (~5 ms)');
+ax = gca;
+ax.XLim = [3000,12000];
+set(gca,'FontSize', 12);
+hold off;
+
+% direction estimate along Y
+figure;
+hold on
+h1 = plot(dirX_);
+h2 = plot(dirX);
+legend([h1 h2], 'Raw', 'Smoothed');
+ylabel('Direction estimate along Y');
+xlabel('Frames (~5 ms)');
+ax = gca;
+ax.XLim = [3000,12000];
+set(gca,'FontSize', 12);
+hold off;
+
+% direction estimate along Z
 figure;
 hold on
 h1 = plot(dirY_);
 h2 = plot(dirY);
-legend([h1 h2], 'smoothed', 'raw');
+legend([h1 h2], 'Raw', 'Smoothed');
+ylabel('Direction estimate along Z');
+xlabel('Frames (~5 ms)');
+ax = gca;
+ax.XLim = [3000,12000];
+set(gca,'FontSize', 12);
 hold off;
 
 %% Smooth optic-flow measurements
-g = gausswin(200,5);
+g = gausswin(200,25);
 g = g/sum(g);
-plot(g);
 
-% CamX_ = CamX;
-% CamY_ = CamY;
-% CamZ_ = CamZ;
-
-% CamX = conv(CamX_, g, 'same');
-% CamY = conv(CamY_, g, 'same');
+CamX = conv(CamX_, g, 'same');
+CamY = conv(CamY_, g, 'same');
 CamZ = conv(CamZ_, g, 'same');
 
-% Plot result
+% renormalize
+cam = normr([CamX, CamY, CamZ]);
+CamX = cam(:,1);
+CamY = cam(:,2);
+CamZ = cam(:,3);
+
+%% Plot results from smoothing of optic-flow estimates
+close all
+% direction estimate along X
+figure;
+hold on
+h1 = plot(CamX_);
+h2 = plot(CamX);
+legend([h1 h2], 'Raw', 'Smoothed');
+ylabel('Direction estimate along X');
+xlabel('Frames (~5 ms)');
+ax = gca;
+ax.XLim = [3000,12000];
+set(gca,'FontSize', 12);
+hold off;
+
+% direction estimate along Y
+figure;
+hold on
+h1 = plot(CamY_);
+h2 = plot(CamY);
+legend([h1 h2], 'Raw', 'Smoothed');
+ylabel('Direction estimate along Y');
+xlabel('Frames (~5 ms)');
+ax = gca;
+ax.XLim = [3000,12000];
+set(gca,'FontSize', 12);
+hold off;
+
+% direction estimate along Z
 figure;
 hold on
 h1 = plot(CamZ_);
 h2 = plot(CamZ);
-legend([h1 h2], 'raw', 'smoothed');
+legend([h1 h2], 'Raw', 'Smoothed');
+ylabel('Direction estimate along Y');
+xlabel('Frames (~5 ms)');
+ax = gca;
+ax.XLim = [3000,12000];
+set(gca,'FontSize', 12);
 hold off;
 
-%% Plot raw measurements
+%% Compare results along each axis
+close all
+% direction estimate along X (absolute value)
 figure;
 hold on
-h2 = plot(abs(CamZ));
-drawnow;
-pause;
+h1 = plot(abs(dirZ));
+h2 = plot(abs(CamX));
+legend([h1 h2], 'Ground truth', 'Estimate');
+ylabel('Direction estimate along X');
+xlabel('Frames (~5 ms)');
+ax = gca;
+ax.XLim = [3000,6000];
+set(gca,'FontSize', 12);
+hold off;
+
+% direction estimate along Y (absolute value)
+figure;
+hold on
+h1 = plot(abs(dirX));
+h2 = plot(abs(CamY));
+legend([h1 h2], 'Ground truth', 'Estimate');
+ylabel('Direction estimate along Y');
+xlabel('Frames (~5 ms)');
+ax = gca;
+ax.XLim = [3000,6000];
+set(gca,'FontSize', 12);
+hold off;
+
+% direction estimate along Z (absolute value)
+figure;
+hold on
 h1 = plot(abs(dirY));
-legend([h1, h2], 'Optitrack Y','Camera Z');
+h2 = plot(abs(CamZ));
+legend([h1 h2], 'Ground truth', 'Estimate');
+ylabel('Direction estimate along Z');
+xlabel('Frames (~5 ms)');
+ax = gca;
+ax.XLim = [3000,6000];
+set(gca,'FontSize', 12);
+hold off;
+
+%% Compute accuracy and statistics
+% define variables 
+dir_gt = [dirZ, dirX, dirY];
+dir_est = [CamX, CamY, CamZ];
+
+% compute error
+accuracy_ = abs(dot(dir_gt', dir_est'));
+
+% compute statistics
+acc_mean = mean(accuracy_);
+acc_std  = std(accuracy_);
+acc_max = max(accuracy_);
+acc_min = min(accuracy_);
+
+%% Plot accuracy and statistics
+figure;
+hold on
+plot(accuracy_)
+ylabel('Accuracy');
+xlabel('Frames (~5 ms)');
+ax = gca;
+ax.XLim = [3000,6000];
+set(gca,'FontSize', 12);
 hold off
 
-% % plot error
-% error = CamZ_'+abs(dirY_);
+% display results
+fprintf('Mean accuracy: %f \n Std. dev.: %f \n Max: %f \n Min: %f \n', acc_mean, acc_std, acc_max, acc_min);
+
+%% Process velocity
+% g = gausswin(200,25);
+% g = g/sum(g);
+% plot(g);
+% 
+% velX_ = velX(1:length(CamZ));
+% velY_ = velY(1:length(CamZ));
+% velZ_ = velZ(1:length(CamZ));
+% 
+% velX = conv(velX_, g, 'same');
+% velY = conv(velY_, g, 'same');
+% velZ = conv(velZ_, g, 'same');
+% 
+% vel = [velX, velY, velZ];
+
+%% Compute curvature of the trajectory
+% timestep = 5*10^-3;
+% 
+% diffR = zeros(length(vel(:,1)), 3);
+% diffR(1,:) = cross(vel(2,:), vel(1,:));
+% for i = 2:length(diffR)-1,
+%     diffR(i,:) = cross(vel(i,:),vel(i-1,:));
+% end
+% diffR(length(diffR),:) = cross(vel(length(diffR),:),vel(length(diffR)-1,:));
+% 
+% curv_ = asin(sqrt(sum(diffR.^2,2)))/timestep;
+% 
+%% Compute estimation error
+% error_ = abs(CamZ') + abs(dirY);
+% 
+%% Compute best parameters for criteria
+% % Parameters
+% N = 500;
+% lambda_ = 0.1:0.01:0.99; % Grid-search
+% 
+% best_cor = 0;
+% best_lambda = 0;
+% 
+% for l = 1:length(lambda_),
+%     lambda = lambda_(l);
+%     
+%     % compute criteria
+%     
+%     num = zeros(length(diffR),1);
+% 
+%     den = 0;
+%     for n = 0:N,
+%         den = den + lambda^n;
+%     end
+% 
+%     for k = 1:length(diffR),
+%         for n = 0:(min(k-1,N)),
+%             num(k) = num(k) + curv_(k-n)*lambda^n;
+%         end
+%     end
+%     crit_ = num/den;
+%     
+%     % compute correlation
+%     corcoeffs = corrcoef(error_, crit_);
+%     cor = abs(corcoeffs(2,1));
+%     if cor > best_cor,
+%         best_cor = cor;
+%         best_lambda = lambda_(l);
+%     end
+% end
+% 
+%% Compute final criteria
+% lambda = best_lambda;
+% num = zeros(length(diffR),1);
+% 
+% den = 0;
+% for n = 0:N,
+%     den = den + lambda^n;
+% end
+% 
+% for k = 1:length(diffR),
+%     for n = 0:(min(k-1,N)),
+%         num(k) = num(k) + curv_(k-n)*lambda^n;
+%     end
+% end
+% crit_ = num/den;
+% 
+%% Plot error as a function of criteria
+% [crit_s, order] = sort(crit_);
 % figure;
 % hold on
-% plot(error);
+% plot(crit_s, error_);
